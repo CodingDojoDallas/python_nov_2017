f521cc2d8a6be809108e0f94a1394624b72c98f3
Andy LaBorde <andikthegreat@gmail.com>
2017-10-30 15:50:18 -0500

    first commit Mult_sum_avg assignment

---
+++ Andy_LaBorde/Mult_Sum_Avg/multSumAvg.py
@@ -0,0 +1,15 @@
+for x in range(1, 1001):
+	if x % 2 == 1:
+		print x
+
+for x in range(5, 1000000):
+	if x % 5 == 0:
+		print x
+
+a = [1,2,5,10,255,3]
+
+print sum(a)
+
+a = [1,2,5,10,255,3]
+
+print sum(a) / len(a)
