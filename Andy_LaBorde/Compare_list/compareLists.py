1a22fdb54f8a70b204909a4bc9dc59889121e4e6
Andy LaBorde <andikthegreat@gmail.com>
2017-10-30 17:36:00 -0500

    compare lists assignment

---
+++ Andy_LaBorde/Compare_list/compareLists.py
@@ -0,0 +1,40 @@
+list_one = [1,2,5,6,2]
+list_two = [1,2,5,6,2]
+
+
+if set(list_one) == set(list_two):
+	print 'the lists are the same'
+else:
+	print 'the listsnare not the same'
+
+
+list_one = [1,2,5,6,5]
+list_two = [1,2,5,6,5,3]
+
+
+if set(list_one) == set(list_two):
+	print 'the lists are the same'
+else:
+	print 'the listsnare not the same'
+
+
+
+list_one = [1,2,5,6,5,16]
+list_two = [1,2,5,6,5]
+
+
+if set(list_one) == set(list_two):
+	print 'the lists are the same'
+else:
+	print 'the listsnare not the same'
+
+
+list_one = ['celery','carrots','bread','milk']
+list_two = ['celery','carrots','bread','cream']
+
+
+if set(list_one) == set(list_two):
+	print 'the lists are the same'
+else:
+	print 'the listsnare not the same'
+
