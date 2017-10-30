4a6bf45b24a23ed5c4369de0281ec6c7012614ca
Brittney <b.nychole87@gmail.com>
2017-10-30 15:52:47 -0500

    updated multiples

--- brittney_jackson/multiples.py
+++ brittney_jackson/multiples.py
@@ -8,3 +8,11 @@ for x in range(1,1001):
 for x in range(5,1000000):
 	if x%5 == 0:
 		print x
+
+# program to print sums of all values in the given list
+a = [1,2,5,10,255,3]
+print sum(a)
+
+# program to average sums in the given list
+a = [1,2,5,10,255,3]
+print sum(a) / len(a)
