79d81d61ab4e57346654f4d131e4b83f36d38f38
richnamk <richnamk@gmail.com>
2017-10-30 22:53:18 -0500

    assignment

--- richardN/frstAssign/helloWorld.py
+++ richardN/frstAssign/helloWorld.py
@@ -81,3 +81,60 @@ my_list = [1, 'Zen', 'hi']
 print len(my_list)
 # output
 3
+'''enumerate(sequence) used in a for loop context to return two-item-tuple for each item in the list indicating the index followed by the value at that index.
+map(function, sequence) applies the function to every item in the sequence you pass in. Returns a list of the results.
+min(sequence) returns the lowest value in a sequence.
+sorted(sequence) returns a sorted sequence
+https://docs.python.org/2/library/functions.html
+'''
+my_list = [1,5,2,8,4]
+my_list.append(7)
+print my_list
+# output:
+# [1,5,2,8,4,7]
+'''
+list.extend(list2) adds all values from a second sequence to the end of the original sequence.
+list.pop(index) remove a value at given position. if no parameter is passed, defaults to final value in the list.
+list.index(value) returns the index position in a list for the given parameter.
+http://www.linuxtopia.org/online_books/programming_books/python_programming/python_ch14s07.html
+
+# if statement:
+if <condition>:
+  # do something
+# if-else statement:
+elif <condition>:
+  # do something
+else:
+  # do this instead
+'''
+age = 21
+if age >= 18:
+  print 'Legal age'
+else:
+  print 'You are so young!'
+if age >= 18:
+  print 'Legal age'
+elif age == 17:
+  print 'You are seventeen.'
+else:
+  print 'You are so young!'
+'''
+ for <counter> in <sequence or range>:
+   do something
+'''
+#create new list remember lists can hold may data types, even lists
+
+my_list = [4, 'dog', 20, ['watt', 'dsw', 99], 'fml']
+for element in my_list:
+    print element
+
+# while <expression>
+for val in "string":
+  if val == "i":
+    break
+  print val
+
+for val in "string":
+  if val == "i":
+    continue
+  print val
