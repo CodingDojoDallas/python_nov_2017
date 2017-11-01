e42a78ebbebff2ebbaef40b88dfa192320931e04
Justin Quiros <jquiros2290@gmail.com>
2017-11-01 12:00:09 -0500

    Bike Change

--- justin_quiros/bike.py
+++ justin_quiros/bike.py
@@ -5,25 +5,25 @@ class Bike(object):
 		self.max_speed =  max_speed
 		self.miles = 0
 	def displayinfo(self):
-		print self.price
-		print self.max_speed
-		print self.miles
+		print "Price is $: " + str(self.price) 
+		print "Max speed is: " + str(self.max_speed) + " mph"
+		print "Total miles: " + str(self.miles) + " miles"
 		return self
 	def ride(self):
 		print "Riding"
-		self.miles = self.miles + 10
+		self.miles += 10
 		return self
 	def reverse(self):
 		print "Reversing"
-		self.miles = self.miles - 5
+		self.miles -= 5
 		if self.miles < 0:
 			self.miles = 0
 		return self
 
 
-Bike1 = Bike(300, "15 mph")
-Bike2 = Bike(250, "10 mph")
-Bike3 = Bike(450, "20 mph")
+Bike1 = Bike(300, "15")
+Bike2 = Bike(250, "10")
+Bike3 = Bike(450, "20")
 
 
 
