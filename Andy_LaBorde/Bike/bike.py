66deb2026aac37762801b30507905b3a26a97dea
Andy LaBorde <andikthegreat@gmail.com>
2017-11-02 13:18:38 -0500

    call center assignment, updated bike assignment

--- Andy_LaBorde/Bike/bike.py
+++ Andy_LaBorde/Bike/bike.py
@@ -1,11 +1,12 @@
 
 class bike(object):
-	def __init__(self, price, max_speed):
+	def __init__(self, name, price, max_speed):
+		self.name = name
 		self.price = price
 		self.max_speed = max_speed
 		self.miles = 0
 	def displayInfo(self):
-		print self.price, self.max_speed, self.miles
+		print self.name, self.price, self.max_speed, self.miles
 		return self
 	def ride(self):
 		print "Riding" 
@@ -18,17 +19,14 @@ class bike(object):
 		return self
 			# self.miles = 0
 
-bike1 = bike(200,50)
+bike1 = bike('bike1', 200,50)
 bike1.displayInfo().ride().ride().ride().reverse().reverse().displayInfo()
 
-bike2 = bike(200, 50)
+bike2 = bike('bike2', 200, 50)
 bike2.displayInfo().ride().ride().reverse().reverse().displayInfo()
 
-bike3 = bike(200, 50)
-bike3.displayInfo()
-bike3.reverse()
-bike3.reverse()
-bike3.displayInfo()
+bike3 = bike('bike3', 200, 50)
+bike3.reverse().reverse().displayInfo()
 
 
 
