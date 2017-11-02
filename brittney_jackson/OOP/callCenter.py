4548a5652c49bafcdcc654c4429b72788ad92851
Brittney <b.nychole87@gmail.com>
2017-11-02 14:07:01 -0500

    update

--- brittney_jackson/OOP/callCenter.py
+++ brittney_jackson/OOP/callCenter.py
@@ -1,20 +1,27 @@
-
+global counter
+counter = 1000
 
 class Call(object):
 
-	def __init__(self, name, num, time, reason):
+	def __init__(self, name, phone, time, reason):
 		self.name = name
-		self.num = num
+		self.phone = phone
 		self.time = time
 		self.reason = reason
+		global counter
+		counter +=1
+		self.gid = counter
+
+
 	
 		#print self.queue
 
 	def display(self):
 		print 'Caller Name: ' +self.name 
-		print 'Caller Number: ' +self.num 
+		print 'Caller Number: ' +self.phone 
 		print 'Call Time: ' +self.time 
 		print 'Call Reason: ' +self.reason 
+		print 'Unique ID: ' +str(self.gid)
 		#print 'Queue Position:' +self.queue 
 
 class CallCenter(object):
@@ -49,6 +56,7 @@ newCall2 = Call('Jena', '555-1001', '2:32pm', 'lost debit card')
 
 cc1 = CallCenter()
 cc1.add(newCall1).add(newCall2).info()
+print '* * * * * * * *'
 cc1.endCall(newCall1).info()
 
 
