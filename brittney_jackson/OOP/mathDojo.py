ef662d7ae0c7dd354bd5656102d0e4fc23a63dd0
Brittney <b.nychole87@gmail.com>
2017-11-02 12:26:02 -0500

    mathdojo

---
+++ brittney_jackson/OOP/mathDojo.py
@@ -0,0 +1,25 @@
+
+
+class mathDojo(object):
+
+	def __init__(self):
+		self.result = 0
+
+	def add(self, *args):
+		for i in args:
+			self.add =self.result + i
+
+	def sub(self, *args):
+		for i in args:
+			self.sub = self.result - i
+		
+
+
+
+md = mathDojo()
+
+md.add(2).add(2,5)
+
+
+
+
