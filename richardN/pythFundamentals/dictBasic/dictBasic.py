c2c1f250489dd28c3387f5ac6ae3f100de408adf
richnamk <richnamk@gmail.com>
2017-11-02 20:45:17 -0500

    dictBasicAssign

---
+++ richardN/pythFundamentals/dictBasic/dictBasic.py
@@ -0,0 +1,8 @@
+me = {'name':'richard','ethn': 'asian','car': 'mini','mOt': 'bike'}
+
+
+def print_dictionary_values(me):
+    for item in me:
+        print me['age']
+
+print_dictionary_values(me)
