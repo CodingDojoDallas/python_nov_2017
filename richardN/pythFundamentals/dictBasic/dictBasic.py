5f393e017c3cc28b48d78c45dbb81271a1471279
richnamk <richnamk@gmail.com>
2017-11-03 21:41:37 -0500

    assignment

--- richardN/pythFundamentals/dictBasic/dictBasic.py
+++ richardN/pythFundamentals/dictBasic/dictBasic.py
@@ -1,8 +1,18 @@
-me = {'name':'richard','ethn': 'asian','car': 'mini','mOt': 'bike'}
+me = {'name':'richard','ethn': 'asian','car': 'mini','mOt': 'bike','age':25}
 
+#
+# def print_dictionary_values(me):
+#     for item in me:
+#         print me['age']
 
-def print_dictionary_values(me):
-    for item in me:
-        print me['age']
+# print_dictionary_values(me)
 
-print_dictionary_values(me)
+print me['name']
+print me.keys() # calls for the keys
+print len(me) # asks for the number of keys
+print str(me) #gives dictionary as a string
+print type(me) # informs you what kind of variable it is..... ie: tuple, int, float, string, etc...
+# .clear() it "clears" the dictionary
+#print me.copy() shallow copy? WTfrack
+print me.items()
+print me.values()
