28fcd249b758c350c93dd701ad666d6035c08e6c
Brittney <b.nychole87@gmail.com>
2017-11-05 17:02:09 -0600

    disappearing ninjas

--- brittney_jackson/flaskAssignments/disappearingNinja/server.py
+++ brittney_jackson/flaskAssignments/disappearingNinja/server.py
@@ -5,19 +5,32 @@ app = Flask(__name__)
 
 
 @app.route('/')          
-
 def landing():
 	return render_template('index.html')    
 
-@app.route('/ninjas')
-
+@app.route('/ninja')
 def ninjas():
   return render_template('ninjas.html')
 
-@app.route('/dojo/now')
+@app.route('/ninja/orange')
+def orange():
+  return render_template('orange.html')
 
-def dojo():
-  return render_template('dojo.html')
+@app.route('/ninja/red')
+def red():
+  return render_template('red.html')
+
+@app.route('/ninja/blue')
+def blue():
+  return render_template('blue.html')
+
+@app.route('/ninja/purple')
+def purple():
+  return render_template('purple.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
 
 
 app.run(debug=True)     
