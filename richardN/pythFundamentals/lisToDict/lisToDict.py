bb060116e1df506796534bb78c1f0515a77f9e00
richnamk <richnamk@gmail.com>
2017-11-04 23:04:47 -0500

    newList

---
+++ richardN/pythFundamentals/lisToDict/lisToDict.py
@@ -0,0 +1,5 @@
+x = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+y = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+
+for nl in range(min(len(x), len(y))):
+    print x[nl], y[nl]
