6dac4729577642ba8356f0ab8e29235a9dda14b0
Justin Quiros <jquiros2290@gmail.com>
2017-11-05 16:41:44 -0600

    Dojo Survey Validation

--- justin_quiros/python_stack/flask_fundamentals/dojo_survey/server.py
+++ justin_quiros/python_stack/flask_fundamentals/dojo_survey/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)`
+from flask import Flask, render_template, request, flash, redirect
+app = Flask(__name__)
+app.secret_key = "ThisIsSecret!"
 
 @app.route('/')
 def index():
@@ -11,10 +12,11 @@ def result():
 	location = request.form['location']
 	language = request.form['language']
 	comments = request.form['comments']
-	# # print name
-	# # print location
-	# print language
-	# print comments
-	return render_template("result.html", name = name, location = location, language = language, comments = comments)
+	if len(request.form['name']) < 1:
+		flash("You did not enter your name.")
+	elif len(request.form['comments']) > 120:
+		flash("This comment box has a 120 character max.")
+	return redirect('/')
+	# return render_template("result.html", name = name, location = location, language = language, comments = comments)
 
 app.run(debug=True)
