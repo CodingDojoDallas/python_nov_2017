4ec111f4bb3efcffd57293b348ac3d14c480e5fc
Erbold <erbold.uran@gmail.com>
2017-11-16 01:03:04 -0600

    Adding my Flask Counter assignment.

--- erbold/Python/Flask/dissNinja/dissNinja.py
+++ erbold/Python/Flask/dissNinja/dissNinja.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
 app = Flask(__name__)
+app.secret_key = random.randit()
 
 @app.route("/")
 def index():
